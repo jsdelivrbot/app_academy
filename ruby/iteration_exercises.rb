e71c6cdb2851183a61f618194f5990f35648861e
App Academy Student <student@appacademy.io>
2017-01-28 19:15:28 -0800

    update iteration exercises

--- ruby/iteration_exercises.rb
+++ ruby/iteration_exercises.rb
@@ -1,83 +1,91 @@
 class Array
-  def my_each
-    i = 0
-    until i == self.length
-      yield(self[i])
-      i += 1
-    end
+
+  def my_each(&prc)
+    self.length.times { |idx| prc.call(self[idx]) }
     self
   end
 
-  def my_select
-    res = []
+  def my_map(&prc)
+    result = []
+    self.my_each { |el| result << prc.call(el) }
+    result
+  end
+
+  def my_select(&prc)
+    result = []
     self.my_each do |el|
-      res << el if yield(el)
+      result << el if prc.call(el)
     end
-    res
+    result
+  end
+
+  def my_inject(acc = nil)
+    acc ||= self.shift
+
+    self.my_each do |el|
+      acc = yield(acc, el)
+    end
+
+    acc
   end
 
   def my_reject
-    res = []
-    self.my_each do |el|
-      res << el unless yield(el)
-    end
-    res
   end
 
   def my_any?
-    self.my_each do |el|
-      return true if yield(el)
-    end
-    false
   end
 
   def my_all?
-    all = true
-    self.my_each do |el|
-      all = false unless yield(el)
-    end
-    all
   end
 
   def my_flatten(res = [])
-    self.my_each do |el|
-      if el.is_a?(Array)
-        el.my_flatten(res)
-      else
-        res << el
-      end
-    end
-    res
   end
 
   def my_zip(*args)
-    res = []
-    self.length.times do |i|
-      res << Array.new(args.length + 1)
-    end
-    args = [self.dup] + args
-
-    args.each_with_index do |array, args_idx|
-      array.each_with_index do |el, array_idx|
-        res[array_idx][args_idx] = el if res[array_idx]
-      end
-    end
-  res
   end
 
   def my_rotate(num = 1)
-    res = num > 0 ? self.dup : self.dup.reverse
-    num.abs.times do
-      res << res.shift
-    end
-    res = num > 0 ? res : res.reverse
   end
 
   def my_join(seperator = "")
-    thing = self.dup
-    last_el = thing.pop
-    string = thing.inject(""){|string, el| string + el + seperator}
-    string + last_el
   end
+
+  def my_reverse
+  end
+
+  # ### Factors
+#
+# Write a method `factors(num)` that returns an array containing all the
+# factors of a given number.
+
+def factors(num)
+end
+
+# ### Substrings and Subwords
+#
+# Write a method, `substrings`, that will take a `String` and return an
+# array containing each of its substrings. Don't repeat substrings.
+# Example output: `substrings("cat") => ["c", "ca", "cat", "a", "at",
+# "t"]`.
+#
+# Your `substrings` method returns many strings that are not true English
+# words. Let's write a new method, `subwords`, which will call
+# `substrings`, filtering it to return only valid words. To do this,
+# `subwords` will accept both a string and a dictionary (an array of
+# words).
+
+def substrings(string)
+end
+
+def subwords(word, dictionary)
+end
+
+# ### Doubler
+# Write a `doubler` method that takes an array of integers and returns an
+# array with the original elements multiplied by two.
+
+def doubler
+end
+
 
 end
