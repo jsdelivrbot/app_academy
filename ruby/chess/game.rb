72d507377373de678c20c1f4cd4c6bc5827891d1
App Academy Student <student@appacademy.io>
2017-01-31 12:30:22 -0800

    add super cool changes to chess

---
+++ ruby/chess/game.rb
@@ -0,0 +1,16 @@
+require_relative 'board'
+require_relative 'display'
+
+class Game#(two players)
+  # ###only allow you to click pieces of current_player color
+  # allow you to select where to move to
+  # move there via Board#move_piece
+    # unset selected pieces in cursor
+  # display removed pieces
+
+
+
+
+  # include swtich player function
+
+end
