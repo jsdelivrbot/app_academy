d9710dd3391ce7e42caf026042d6ed5cbd51382d
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-04 11:27:30 -0800

    save

--- HTML_CSS/AppAcademyTimes/app/assets/javascripts/components/dropdown.js
+++ HTML_CSS/AppAcademyTimes/app/assets/javascripts/components/dropdown.js
@@ -3,6 +3,7 @@
 // dropdown function that toggles 'hidden' class to the gear-dropdown ul
 const toggleDropdown = () => {
 	$('#gear-dropdown').toggleClass('hidden');
+	$('#gear-dropdown').toggleClass('dropdown-border-effects');
 };
 
 // Add click listener to gear icon which invokes toggle function
