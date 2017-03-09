f6192744f830a1319ed1111120ad5eb9fbb42006
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-09 11:28:43 -0800

    warmup with codewars-js

--- js/codewars-js/codewars.js
+++ js/codewars-js/codewars.js
@@ -3,14 +3,58 @@
 // ----------
 
 // Write function replaceAll that will replace all occuriencies of
-// an item with another.
+// an item with another. Should work and both string and array inputs
 //
 // Ex: replaceAll([1,2,2], 1, 2) => [2,2,2]
 
-const replaceAll = (arr, num, sub) => {
+const replaceAll = (input, num, sub) => {
+  return Array.isArray(input)
+    ? input.map( el => el === num ? sub : el )
+    : input.replace(RegExp(num, 'g'), sub);
+}; 
+
+// ----------
+// ~ 6 kyu ~
+// ----------
+
+const sqInRect = (w, h, firstRound = true) => {
+  if (w > h) {
+    let oldW = w;
+    w = h;
+    h = oldW;
+  }
+
   let result = [];
-  arr.forEach( el => {
-    result.push(el === num ? sub : el);
+
+  if ( w === 1 ){
+    for (var i = 0; i < h; i++) {
+      result.push(w);
+    }
+  } else if ( w > 1 ) {
+    result.push(w);
+    result.push(sqInRect((h - w), w, false));
+  }
+
+  return ((w === h) && firstRound) ? null : result.flatten();
+
+};
+
+Array.prototype.flatten = function () {
+  let result = [];
+  this.forEach( el => {
+    if(el){
+      if( typeof el === "number") {
+        result.push(el);
+      } else {
+        result = result.concat(el.flatten());
+      }
+    }
   });
   return result;
 };
+
+console.log(sqInRect(5, 5));
+console.log(sqInRect(5, 3));
+console.log(sqInRect(16, 31));
+console.log(sqInRect(4, 16));
+// console.log([ 3, [ 2, [ 1, 1 ] ] ].flatten());
