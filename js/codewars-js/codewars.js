8f3f0a8c8687a0cf803111fd3c1ca572984c4b92
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-10 10:21:01 -0800

    complete maxAndMin codewars-js challenge

--- js/codewars-js/codewars.js
+++ js/codewars-js/codewars.js
@@ -1,21 +1,36 @@
-// ----------
-// ~ 7 kyu ~
-// ----------
+// ------------------------------
+//            ~ 6 kyu ~
+// ------------------------------
 
-// Write function replaceAll that will replace all occuriencies of
-// an item with another. Should work and both string and array inputs
+// Given two array of integers(arr1,arr2). Your task is going to find a
+// pair of numbers(an element in arr1, and another element in arr2), their
+// difference is as big as possible(absolute value); Again, you should to
+// find a pair of numbers, their difference is as small as possible.
+// Return the maximum and minimum difference values by an array:
+// [ max difference, min difference ]
 //
-// Ex: replaceAll([1,2,2], 1, 2) => [2,2,2]
+// For example:
+//
+// Given arr1 = [3,10,5], arr2 = [20,7,15,8]
+// should return [17,2] because 20 - 3 = 17, 10 - 8 = 2
 
-const replaceAll = (input, num, sub) => {
-  return Array.isArray(input)
-    ? input.map( el => el === num ? sub : el )
-    : input.replace(RegExp(num, 'g'), sub);
-}; 
 
-// ----------
-// ~ 6 kyu ~
-// ----------
+const maxAndMin = function(arr1, arr2) {
+  let allDiffs = [];
+
+  for (var i1 = 0; i1 < arr1.length; i1++) {
+    for (var i2 = 0; i2 < arr2.length; i2++) {
+      allDiffs.push(Math.abs(arr1[i1] - arr2[i2]));
+    }
+  }
+
+  return [Math.max(...allDiffs), Math.min(...allDiffs)];
+};
+
+console.log(maxAndMin([1,2,3,4,5],[6,7,8,9,10])); //[9,1]
+console.log(maxAndMin([3,10,5],[3,10,5])); // [7,0]
+console.log(maxAndMin([3],[20])); //  [17,17]
+
 
 const sqInRect = (w, h, firstRound = true) => {
   if (w > h) {
@@ -53,8 +68,22 @@ Array.prototype.flatten = function () {
   return result;
 };
 
-console.log(sqInRect(5, 5));
-console.log(sqInRect(5, 3));
-console.log(sqInRect(16, 31));
-console.log(sqInRect(4, 16));
-// console.log([ 3, [ 2, [ 1, 1 ] ] ].flatten());
+// console.log(sqInRect(5, 5));
+// console.log(sqInRect(5, 3));
+// console.log(sqInRect(16, 31));
+// console.log(sqInRect(4, 16));
+
+// ------------------------------
+//            ~ 7 kyu ~
+// ------------------------------
+
+// Write function replaceAll that will replace all occuriencies of
+// an item with another. Should work and both string and array inputs
+//
+// Ex: replaceAll([1,2,2], 1, 2) => [2,2,2]
+
+const replaceAll = (input, find, replace) => {
+  return Array.isArray(input)
+    ? input.map( el => el === find ? replace : el )
+    : input.replace(RegExp(find, 'g'), replace);
+};
---
+++ js/my_jquery/main.js
@@ -0,0 +1,2 @@
+var q = $("ul");
+console.log(q);
