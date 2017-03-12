e350de89fea032d1b5deefe893914f86d963312f
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-11 17:37:38 -0800

    add a bunch of functionality to nicQuery

--- js/my_jQuery/lib/main.js
+++ js/my_jQuery/lib/main.js
@@ -4,7 +4,8 @@
   global.nic$ = arg => {
     //if the arg is a string, treat as a CSS selector and return matching nodes on the page
     if(typeof arg === "string"){
-      let HTMLelementsArr = Array.from(document.querySelectorAll(arg));
+      let HTMLelementsNodeList = document.querySelectorAll(arg);
+      let HTMLelementsArr = Array.from(HTMLelementsNodeList);
       return new DOMNodeCollection(HTMLelementsArr);
     } else if (arg instanceof HTMLElement) {
       return new DOMNodeCollection([arg]);
--- js/my_jQuery/lib/nicQuery.js
+++ js/my_jQuery/lib/nicQuery.js
@@ -50,7 +50,8 @@
 	  global.nic$ = arg => {
 	    //if the arg is a string, treat as a CSS selector and return matching nodes on the page
 	    if(typeof arg === "string"){
-	      let HTMLelementsArr = Array.from(document.querySelectorAll(arg));
+	      let HTMLelementsNodeList = document.querySelectorAll(arg);
+	      let HTMLelementsArr = Array.from(HTMLelementsNodeList);
 	      return new DOMNodeCollection(HTMLelementsArr);
 	    } else if (arg instanceof HTMLElement) {
 	      return new DOMNodeCollection([arg]);
@@ -76,43 +77,97 @@
 	  }
 
 	  html(text){
+	    if (typeof text === "string") {
+	      this.each( node => {
+	        node.innerHTML = text;
+	      });
+	    } else {
+	      return this.HTMLelementsArr[0].innerHTML;
+	    }
+	  }
+
+	  empty(){
+	    this.html("");
+	  }
+
+	  append(child){
+	    if (child instanceof DOMNodeCollection) {
+	      this.append(child.HTMLelementsArr[0].outerHTML);
+	    } else if (typeof child === "object") {
+	      this.each( node => {
+	        let childClone = child.cloneNode(true);
+	        node.appendChild(childClone);
+	      });
+	    } else if (typeof child === "string") {
+	      this.each( node => {
+	        node.insertAdjacentHTML('beforeend', child);
+	      });
+	    }
+	  }
+
+	  attr(attributeName, value = null) {
+	    if (value) {
+	      this.each( node => node.setAttribute(attributeName, value));
+	      return this.HTMLelementsArr;
+	    } else {
+	      return this.HTMLelementsArr[0].getAttribute(attributeName);
+	    }
+	  }
+
+	  addClass(newClass){
 	    this.each( node => {
-	      node.innerHTML = text || "";
+	      node.className = node.className
+	        ? node.className + " " + newClass
+	        : newClass;
+	    });
+	  }
+
+	  removeClass(oldClass){
+	    this.each( node => {
+	      let allClasses = node.className.split(" ");
+
+	      if (allClasses.includes(oldClass)) {
+	        if (allClasses.length === 1 ) {
+	          node.removeAttribute("class");
+	        } else if (allClasses.length > 1) {
+	          let index = allClasses.indexOf(oldClass);
+	          node.className = allClasses.splice(oldClass, 1).join(" ");
+	        }
+	      }
+
+	    });
+	  }
+
+	  children(){
+	    let childrenList = [];
+
+	    this.each( node => {
+	      let childrenNodes = node.children;
+	      childrenList = childrenList.concat(Array.from(childrenNodes));
+	    });
+
+	    return new DOMNodeCollection(childrenList);
+	  }
+
+	  parent(){
+	    let parentsList = [];
+
+	    this.each( node => {
+	      if(!parentsList.includes(node.parentNode)){
+	        parentsList = parentsList.concat(node.parentNode);
+	      }
+	    });
+
+	    return new DOMNodeCollection(parentsList);
+	  }
+
+	  on(eventName, callback){
+	    this.each( node => {
+	      node.addEventListener(eventName, callback);
 	    });
 	  }
 
 	}
-
-	// html
-	//
-	// Let's write the method html first. It can optionally receive a string as a parameter.
-	// If it receives an argument, this will become the innerHTML (hint hint) of the each of the nodes. If it does not receive an argument, it should return the innerHTML of the first node in the array.
-	// empty
-	//
-	// This method clears out the content of all nodes in the internal array. I set the html of all nodes to an empty string.
-	// append
-	//
-	// Take a look here. This method should accept either a jQuery-lite wrapped collection, an HTML element, or a string. Append the outerHTML of each element in the argument to the innerHTML of each element in the DOMNodeCollection. Don't worry about converting strings into HTML elements; just pass them straight through to the elements' innerHTML.
-	// other methods
-	//
-	// I will leave it up to you to figure out ways to implement attr, addClass, and removeClass. All the information for how to change nodes is available in this resource.
-	// traversal
-	//
-	// children
-	//
-	// children is a method that should return a DOMNodeCollection of ALL children of all nodes in the array.
-	// Each node in the array will natively have a children attribute. Look here for more information.
-	// Make sure the return value of this method is an instance of DOMNodeCollection.
-	// parent
-	//
-	// Return a DOMNodeCollection of the parents of each of the nodes
-	// find
-	//
-	// Returns a DOMNodeCollection of all the nodes matching the selector passed in as an argument that are descendants of the nodes. This might come in handy.
-	// remove
-	//
-	// This should remove the html of all the nodes in the array from the DOM
-	// It should also remove all nodes from the array.
 
 	module.exports = DOMNodeCollection;
 
