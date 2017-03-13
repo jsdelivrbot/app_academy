25ee3a84f877593c74e44fe77c27746247510948
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-13 12:42:53 -0700

    complete udemy react course through lecture 25

--- react/react_starter_project/src/index.js
+++ react/react_starter_project/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyD3UxNbOVMcjlHUPBD37eybsFYWFJGVJg0';
 
@@ -23,6 +24,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
