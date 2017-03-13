93d86139f4adf15923c99413f33d8f52ac86b15a
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-13 10:55:24 -0700

    complete udemy react course up until section 1, lecture 16

---
+++ react/react_starter_project/src/components/search_bar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// //functional compoonent
+// const SearchBar = () => {
+//   return <input />;
+// };
+
+//class-based component
+class SearchBar extends Component{
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
--- react/react_starter_project/src/index.js
+++ react/react_starter_project/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar.js';
+
 const API_KEY = 'AIzaSyD3UxNbOVMcjlHUPBD37eybsFYWFJGVJg0';
 
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 React.render(<App />, document.querySelector('.container'));
