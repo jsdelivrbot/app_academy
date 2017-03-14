5b47a9d07108bcdd1d017d458c4dba460e05c2ac
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-14 16:44:05 -0700

    complete book app with redux

--- react/BooksApp/src/actions/index.js
+++ react/BooksApp/src/actions/index.js
@@ -1,3 +1,7 @@
-export default function selectBook(book) {
-  console.log('a book has been selected:', book.title);
+export function selectBook(book) {
+  // action always contains a type, sometimes contains a payload
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
--- react/BooksApp/src/reducers/reducer_books.js
+++ react/BooksApp/src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function(){
   return [
-    {title: 'Eloquent Ruby'},
-    {title: 'The Well-Grounded Rubyist'},
-    {title: 'JavaScript: The Good Parts'},
-    {title: 'Refactoring: Improving the Design of Existing Code'},
-    {title: 'Introduction to Algorithms'}
+    {title: 'Eloquent Ruby', pages: 234 },
+    {title: 'The Well-Grounded Rubyist', pages: 12 },
+    {title: 'JavaScript: The Good Parts', pages: 34 },
+    {title: 'Refactoring: Improving the Design of Existing Code', pages: 56 },
+    {title: 'Introduction to Algorithms', pages: 396 }
   ];
 }
