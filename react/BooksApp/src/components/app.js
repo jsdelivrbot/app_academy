05172152a32546dadc3cd42ebc587cb84f3b73b9
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-14 14:38:01 -0700

    compelte udemy react course through lecture 45

---
+++ react/BooksApp/src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import BooksList from '../containers/books_list'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BooksList />
+      </div>
+    );
+  }
+}
---
+++ react/BooksApp/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+  return [
+    {title: 'Eloquent Ruby'},
+    {title: 'The Well-Grounded Rubyist'},
+    {title: 'JavaScript: The Good Parts'},
+    {title: 'Refactoring: Improving the Design of Existing Code'},
+    {title: 'Introduction to Algorithms'}
+  ];
+}
