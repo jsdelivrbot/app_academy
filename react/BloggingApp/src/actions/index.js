fae57c77a500f41b89a69000d6823cdce9b14364
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-17 13:52:48 -0700

    add things

--- react/BloggingApp/src/actions/index.js
+++ react/BloggingApp/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-const ROOT_URL = 'http;//reduxblog.herokuapp.com/api';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=asdfasdf';
 
 export function fetchPosts() {
