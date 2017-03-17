f2490fdb6d43fe236db39668bfb23a7256426663
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-17 13:03:49 -0700

    add action

--- react/BloggingApp/src/index.js
+++ react/BloggingApp/src/index.js
@@ -4,11 +4,15 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
+import routes from './routes';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={ browserHistory } />
+    <Router history={ browserHistory } routes={ routes }/>
   </Provider>
   , document.querySelector('.container'));
