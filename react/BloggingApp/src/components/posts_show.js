8c88aa7302634b875a5ca37b252587236ba79b9e
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-18 15:46:38 -0700

    add delete post btn

--- react/BloggingApp/src/components/posts_show.js
+++ react/BloggingApp/src/components/posts_show.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsShow extends Component {
   componentWillMount() {
     this.props.fetchPost(this.props.params.id); //pulls id from url, passes it to fetchPost which makes backend request which returns data
+  }
+
+  onDeleteClick(){
+    this.props.deletePost(this.props.params.id);
   }
 
   render() {
@@ -18,6 +22,14 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to='/'>Return to main</Link>
+
+        <button
+          className='btn btn-danger pull-xs-right'
+          onClick={ this.onDeleteClick.bind(this) }
+        >
+          Delete Post
+        </button>
+
         <h3>{ post.title }</h3>
         <h6>Categories:{ post.categories }</h6>
         <p>{ post.content }</p>
@@ -30,4 +42,4 @@ function mapStateToProps(state) {
   return{ post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
