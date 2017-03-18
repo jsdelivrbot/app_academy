ecfcc57a9ee62fe668d599ce4d1c46972a8a4b53
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-18 15:40:15 -0700

    add return to main link

--- react/BloggingApp/src/components/posts_show.js
+++ react/BloggingApp/src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
   componentWillMount() {
@@ -15,7 +16,8 @@ class PostsShow extends Component {
       return <div>Loading...</div>;
     }
     return (
-      <div>Show post { this.props.params.id }
+      <div>
+        <Link to='/'>Return to main</Link>
         <h3>{ post.title }</h3>
         <h6>Categories:{ post.categories }</h6>
         <p>{ post.content }</p>
