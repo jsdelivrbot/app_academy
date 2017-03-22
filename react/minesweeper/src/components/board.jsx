b850bfbc81a0ededb5e37f3e74d7be842f15f496
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-22 14:24:34 -0700

    complete minesweeper through phase 1, Game

---
+++ react/minesweeper/src/components/board.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Tile from './tile';
+
+// The Board component will pass the updateGame function to each individual tile: ultimately, this is how we'll make sure that the board re-renders when the state of the game changes.
+
+class Board extends Component {
+  render(){
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Board;
---
+++ react/minesweeper/src/components/game.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Minesweeper from '../minesweeper';
+import Board from './board';
+
+class Game extends Component {
+  constructor(props){
+    super(props);
+
+    this. state = {
+      board: new Minesweeper.Board(9, 10)
+    };
+
+    this.updateGame = this.updateGame.bind(this);
+  }
+
+  updateGame(){
+    // Stub out a second function, updateGame, but don't write anything here yet. We'll return to this after we have a properly rendering board.
+  }
+
+  render(){
+  return (
+    <div>
+      <Board board={this.state.board} updateGame={this.updateGame} />
+    </div>
+  )
+  }
+}
+
+export default Game;
