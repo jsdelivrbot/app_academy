022bed771b34b25cd7fded111d2596bc0043185b
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-24 16:38:22 -0700

    snake case things

--- react/BooksApp/src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export function selectBook(book) {
-  // action always contains a type, sometimes contains a payload
-  return {
-    type: 'BOOK_SELECTED',
-    payload: book
-  };
-}
--- react/BooksApp/src/reducers/reducer_books.js
+++
@@ -1,9 +0,0 @@
-export default function(){
-  return [
-    {title: 'Eloquent Ruby', pages: 234 },
-    {title: 'The Well-Grounded Rubyist', pages: 12 },
-    {title: 'JavaScript: The Good Parts', pages: 34 },
-    {title: 'Refactoring: Improving the Design of Existing Code', pages: 56 },
-    {title: 'Introduction to Algorithms', pages: 396 }
-  ];
-}
