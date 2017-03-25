ed9f75445ae87373425e2a6cebb8ee56e4ebc554
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-25 00:04:35 -0700

    add initial state

--- react/todo_list/frontend/reducers/todos_reducer.js
+++ react/todo_list/frontend/reducers/todos_reducer.js
@@ -1,6 +1,22 @@
 import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
 
-export default function(state = {}, action) {
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  },
+};
+
+
+export default function(state = initialState, action) {
   Object.freeze(state);
   let nextState;
 
