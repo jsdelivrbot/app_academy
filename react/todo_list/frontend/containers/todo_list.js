bb30699c4d7de0273613516fa4cbdd0724f37e23
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-25 10:30:19 -0700

    write mapStateToProps and mapDispatchToProps

--- react/todo_list/frontend/containers/todo_list.js
+++ react/todo_list/frontend/containers/todo_list.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { allTodos } from '../reducers/selectors';
+import { receiveTodos, receiveTodo } from '../actions/todo_actions';
 
-export default class TodoList extends Component {
+class TodoList extends Component {
   render(){
     return (
       <div>
@@ -9,3 +12,23 @@ export default class TodoList extends Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    todos: allTodos(state),
+    state: state
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    receiveTodo: todo => {
+      dispatch(receiveTodo(todo));
+    },
+    receiveTodos: () => {
+      dispatch(receiveTodos());
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
