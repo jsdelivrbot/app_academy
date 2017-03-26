38e99fa8aeba8a3ed9b3f6c050411ceb1896327f
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-26 11:28:59 -0700

    make things prettier

--- react/todo_list/frontend/components/todo_form.js
+++ react/todo_list/frontend/components/todo_form.js
@@ -52,7 +52,7 @@ class TodoForm extends Component {
             />
         </label>
         <br />
-        <button type='submit'>submit</button>
+        <button className='submit-btn' type='submit'>submit</button>
       </form>
     );
   }
