9090538ab5baba4c9cebd9071adf71ab01b38a43
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-26 13:15:44 -0700

    create StepListItem component and render it into StepList

---
+++ react/todo_list/frontend/components/step_list_item.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class StepListItem extends Component {
+  render(){
+    return (
+      <div>
+        <li>step list item</li>
+      </div>
+    );
+  }
+}
--- react/todo_list/frontend/containers/steps_list.js
+++ react/todo_list/frontend/containers/steps_list.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { allSteps } from '../reducers/selectors';
+import { receiveSteps, receiveStep, removeStep } from '../actions/todo_actions';
+import StepListItem from '../components/step_list_item';
 
 export default class StepsList extends Component {
   render(){
     return(
       <div className={ this.props.className }>
         <ul className='steps-list'>
-          <li>asdf</li>
-          <li>asdf</li>
-          <li>asdf</li>
+          <StepListItem />
         </ul>
       </div>
     );
