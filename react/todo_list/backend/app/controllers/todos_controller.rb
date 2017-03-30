15c16dafe084fa23bbfaa3b34a4467bc1a67972e
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-30 00:40:33 -0700

    write todos_controller that renders json and looks like it might work

--- react/todo_list/backend/app/controllers/todos_controller.rb
+++ react/todo_list/backend/app/controllers/todos_controller.rb
@@ -1,13 +1,36 @@
 class TodosController < ApplicationController
   def new
+    render json: Todo.new
   end
 
   def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
   end
 
   def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    render new
   end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title, :body)
+  end
+  
 end
