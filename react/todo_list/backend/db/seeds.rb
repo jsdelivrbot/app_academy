24cc8ad438a7d74014d370f06960720dd88f1095
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-30 10:27:19 -0700

    seed db

--- react/todo_list/backend/db/seeds.rb
+++ react/todo_list/backend/db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+todo0 = Todo.create!(
+  title: "Drink kombucha",
+  body: "so that my stomach will be happy",
+  done: false,
+  stepsHidden: true,
+  stepFormHidden: true
+)
+
+todo1 = Todo.create!(
+  title: "Drink coffee",
+  body: "so that I will be happy",
+  done: false,
+  stepsHidden: true,
+  stepFormHidden: true
+)
+
+todo2 = Todo.create!(
+  title: "eagerly await Dane getting here",
+  body: ":)",
+  done: false,
+  stepsHidden: true,
+  stepFormHidden: true
+)
+
+todo3 = Todo.create!(
+  title: "think about writing impact statements",
+  body: "more likely just throw a few bullet points into a word doc, call it a day, then try to focus on forgetting about the patriarchy before my stomach explodes",
+  done: false,
+  stepsHidden: true,
+  stepFormHidden: true
+)
+
+todo4 = Todo.create!(
+  title: "sit up straight",
+  body: "so my posture gets better",
+  done: false,
+  stepsHidden: true,
+  stepFormHidden: true
+)
+
+todo5 = Todo.create!(
+  title: "run",
+  body: "then eat",
+  done: false,
+  stepsHidden: true,
+  stepFormHidden: true
+)
+
+todo6 = Todo.create!(
+  title: "dinner with Chanel",
+  body: "Chanel's the best.",
+  done: false,
+  stepsHidden: true,
+  stepFormHidden: true
+)
