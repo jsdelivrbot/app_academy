77d98d023185674ed1b459619c0bd946d6ed18d1
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-30 10:20:30 -0700

    set up static pages controller and view

--- react/todo_list/backend/config/routes.rb
+++ react/todo_list/backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages_controller/root'
+
+  root to: "static_pages#root"
+  
   namespace :api, defaults: {format: :json} do
     resources :todos, only: [:index, :show, :create, :destroy, :update]
   end
