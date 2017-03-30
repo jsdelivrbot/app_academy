7d783c0d25dd92c2fb3395dc412cf3076ac8fe13
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-29 18:00:30 -0700

    refactor

--- react/todo_list/frontend/todo_redux.js
+++ react/todo_list/frontend/todo_redux.js
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { allTodos, allSteps } from './reducers/selectors';
-import { receiveTodos, receiveTodo, removeTodo } from './actions/todo_actions';
-
-window.store = configureStore;
-window.allSteps = allSteps;
-window.allTodos = allTodos;
-window.receiveTodo = receiveTodo;
-window.receiveTodos = receiveTodos;
-window.removeTodo = receiveTodos;
-
 
 document.addEventListener("DOMContentLoaded", function(){
   ReactDOM.render(<Root store={ configureStore }/>, document.getElementById('root'));
