7f43e1212e0630cc7d75d406528bf9ae13d76065
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-30 09:59:57 -0700

    set up routes

--- react/todo_list/backend/config/routes.rb
+++ react/todo_list/backend/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'todos/new'
-
-  get 'todos/create'
-
-  get 'todos/update'
-
-  get 'todos/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :todos, only: [:index, :show, :create, :destroy, :update]
+  end
 end
