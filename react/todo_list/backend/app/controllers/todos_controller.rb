11c756f4d0f17130a328867cb16ac8ced76d632d
Nicole Hemenway <nkhemenway@gmail.com>
2017-03-30 00:33:40 -0700

    create todos controller and todos views

---
+++ react/todo_list/backend/app/controllers/todos_controller.rb
@@ -0,0 +1,13 @@
+class TodosController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- react/todo_list/backend/config/routes.rb
+++ react/todo_list/backend/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'todos/new'
+
+  get 'todos/create'
+
+  get 'todos/update'
+
+  get 'todos/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
