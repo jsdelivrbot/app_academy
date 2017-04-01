c79e9ac13f1a4578306c7c7211a132776b9395d9
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-01 13:25:39 -0700

    write receiveSearchGiphys actions, giphysReducer, and rootReducer

--- react/giphy_search/giphy_search.jsx
+++ react/giphy_search/giphy_search.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { fetchSearchGiphys } from './util/api_util';
-
-window.fetchSearchGiphys = fetchSearchGiphys;
 
 document.addEventListener("DOMContentLoaded", function(){
   ReactDOM.render(<Root />, document.getElementById('root'));
