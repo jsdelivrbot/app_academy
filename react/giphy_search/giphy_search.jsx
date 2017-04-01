bf35a631add0d962b2f4f089772a9c27f01ea0ff
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-01 14:12:11 -0700

    apply thunk middleware to action creator

--- react/giphy_search/giphy_search.jsx
+++ react/giphy_search/giphy_search.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import { receiveSearchGiphys, fetchSearchGiphys } from './actions/giphy_actions';
+
+window.store = configureStore();
+window.fetchSearchGiphys = fetchSearchGiphys;
+window.receiveSearchGiphys = receiveSearchGiphys;
 
 document.addEventListener("DOMContentLoaded", function(){
-  ReactDOM.render(<Root />, document.getElementById('root'));
+  ReactDOM.render(<Root store={ configureStore } />, document.getElementById('root'));
 });
-
-// store={ configureStore }/>
