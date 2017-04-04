8386783534afe2ae047110d47c4b4e2c22193254
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-04 14:33:23 -0700

    pass more specs

--- react/jbuilder/config/routes.rb
+++ react/jbuilder/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :gifts, only: :show
+
     resources :guests, only: [:index, :show] do
-       resources :gifts, only: :index
+      resources :gifts, only: :index
     end
+
+    resources :gifts, only: :show
+    resources :parties, only: [:index, :show]
+    
   end
 
-  resources :parties, only: [:index, :show]
 end
