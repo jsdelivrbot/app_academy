ee4103f4931f59b5cb1a8b9325abfa351425743c
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-05 11:31:44 -0700

    apply middleware

--- react/pokedex/frontend/store/store.js
+++ react/pokedex/frontend/store/store.js
@@ -1,13 +1,10 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import { applyMiddleware } from 'redux';
+import thunkFunc from '../middleware/thunk';
 
-const configureStore = () => createStore(rootReducer);
+const configureStore = createStore(
+  rootReducer, applyMiddleware(thunkFunc)
+);
 
 export default configureStore;
-
-// );
-// import { applyMiddleware } from 'redux';
-// import thunkFunc from '../middleware/thunk';
-//
-// export default const configureStore = createStore(
-//   rootReducer, applyMiddleware(thunkFunc)
