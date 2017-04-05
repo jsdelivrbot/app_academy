b17afaa24be86def24765174c0af5d61ef61a873
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-04 17:16:34 -0700

    start writign actions

--- react/pokedex/frontend/pokedex.js
+++ react/pokedex/frontend/pokedex.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { receiveAllPokemon, RECEIVE_ALL_POKEMON } from './actions/pokemon_actions';
+import { fetchAllPokemon } from './util/api_util';
+
+window.fetchAllPokemon = fetchAllPokemon;
+window.receiveAllPokemon = receiveAllPokemon;
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
--- react/pokedex/frontend/util/api_util.js
+++ react/pokedex/frontend/util/api_util.js
@@ -1,20 +1,6 @@
-// Inside this file, we'll define functions that make ajax requests fetching information from our rails api.
-// Export a function called fetchAllPokemon that returns a promise.
-// The function should make an AJAX request that will make a http request to our PokemonController#index endpoint.
-// Run rake routes to determine the appropriate url for this request.
-
-
 export const fetchAllPokemon = () => (
   $.ajax({
     method: 'GET',
     url: 'api/pokemon'
   })
 );
-// 
-// export const createTodo = todo => {
-//   return $.ajax({
-//     method: 'POST',
-//     url: 'api/todos',
-//     data: todo
-//   });
-// };
