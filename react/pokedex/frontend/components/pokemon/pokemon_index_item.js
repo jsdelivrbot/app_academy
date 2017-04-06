2472fd6c445eaf63f00488c159e82bf8989da08a
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-06 10:51:25 -0700

    add link to PokemonIndexItem

--- react/pokedex/frontend/components/pokemon/pokemon_index_item.js
+++ react/pokedex/frontend/components/pokemon/pokemon_index_item.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const PokemonIndexItem = ({pokemon}) => (
     <li key={ pokemon.id } className="poke-li">
-      <h3>{ pokemon.name }</h3>
-      <p><img src={pokemon.image_url} /></p>
+      <Link to={`/pokemon/${pokemon.id}`}>
+        <h3>{ pokemon.name }</h3>
+        <p><img src={pokemon.image_url} /></p>
+      </Link>
     </li>
   );
 
