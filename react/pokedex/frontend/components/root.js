8c67cfe639bd6c788648179a4837dc3731e91f4f
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-06 00:17:23 -0700

    start adding react-router

--- react/pokedex/frontend/components/root.js
+++ react/pokedex/frontend/components/root.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, hashHistory } from 'react-router';
 import PokemonIndex from '../containers/pokemon/pokemon_index';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <PokemonIndex />
+    <Router history={hashHistory}>
+      <Route path="/" component={ PokemonIndex } />
+    </Router>
   </Provider>
 );
 
