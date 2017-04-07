eaefa8dfc3dd645150c2b6c4a5b5e52c595eaff6
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-07 16:21:13 -0700

    render AlbumList in index

--- react/albums/index.ios.js
+++ react/albums/index.ios.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { Text, AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { View, Text, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => {
   return (
-    <Header />
+    <View>
+      <Header headerText={'Albums'}/>
+      <AlbumList />
+    </View>
   );
 };
 
