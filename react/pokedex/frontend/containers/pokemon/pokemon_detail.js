acf959356f20077d7a28cab9f036038dd4b7b30c
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-06 17:31:55 -0700

    get as far as im going to today

--- react/pokedex/frontend/containers/pokemon/pokemon_detail.js
+++ react/pokedex/frontend/containers/pokemon/pokemon_detail.js
@@ -4,30 +4,32 @@ import { requestSinglePokemon } from '../../actions/pokemon_actions';
 import { bindActionCreators } from 'redux';
 
 class PokemonDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemon: props.pokemon,
+      pokemonDetail: props.pokemonDetail
+    };
+
+  }
 
   componentDidMount(){
-    console.log('in pokemon_detail componentDidMount:');
-    console.log(this.props.match.params.pokemonId);
     this.props.requestSinglePokemon(this.props.match.params.pokemonId);
   }
 
-  componentWillReceiveProps(newProps){
-    console.log('in pokemon_detail componentWillReceiveProps:');
-    console.log(newProps);
-    this.props.requestSinglePokemon(newProps.params.pokemonId);
+  componentWillReceiveProps(nextProps){
+    if (this.props.params.pokemonId !== nextProps.params.pokemonId) {
+      this.props.requestSinglePokemon(nextProps.params.pokemonId);
+    }
   }
 
   render(){
-    const pokeAttributes = this.props.pokemonDetail;
-    // const pokeDetailData = this.props.pokemonDetail[0];
-    // const pokeIds = Object.keys(pokeData);
-    // const pokemonItems = pokeIds.map(pokeId =>
-    //   <PokemonIndexItem key={pokeId} pokemon={pokeData[pokeId]} />
-    // );
     return (
       <section className="poke-detail">
         <p>poke-detail</p>
         <p>{this.props.match.params.pokemonId}</p>
+        <p>{console.log(this.props)}</p>
       </section>
     );
   }
