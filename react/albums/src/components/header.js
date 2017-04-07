c9529b8a717b3c0868c234d33eb5e3aa5dbb6010
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-07 15:52:32 -0700

    add styling in header

--- react/albums/src/components/header.js
+++ react/albums/src/components/header.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const Header = () => {
+  const { textStyle } = styles;
   return (
-    <Text>Albums</Text>
+    <Text style={ textStyle }>Albums</Text>
   );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
 };
 
 export default Header;
