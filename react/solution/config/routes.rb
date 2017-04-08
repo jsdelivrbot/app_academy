cb3ddf4d03dda4417e2e5463461aa83bf977de09
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-08 16:42:47 -0700

    set up backend

--- react/solution/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'static_pages#root'
-
-  namespace :api, defaults: {format: :json} do
-    resources :pokemon, only: [:create, :destroy, :index, :show, :update]
-  end
-end
