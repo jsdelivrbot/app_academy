cdd94c7eb9eb7797e43be4a1a0177516006f0f4a
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-09 16:55:37 -0700

    write session action creators

---
+++ react/bench_bnb/frontend/actions/session_actions.js
@@ -0,0 +1,70 @@
+import * as ApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const signUp = user => dispatch => {
+  ApiUtil.signUp(user)
+    .then(user => dispatch(receiveCurrentUser(user)),
+    errors => console.log(errors))
+}
+
+// export const signup = user => dispatch => (
+//   APIUtil.signup(user)
+//     .then(user => dispatch(receiveCurrentUser(user)),
+//       err => dispatch(receiveErrors(err.responseJSON)))
+// );
+//
+
+export const signIn = user => dispatch => {
+  ApiUtil.signIn(user)
+    .then(user => {
+      dispatch(receiveCurrentUser(user);
+    }), errors => {
+      console.log(errors);
+    })
+}
+
+// export const login = user => dispatch => (
+//   APIUtil.login(user)
+//     .then(user => dispatch(receiveCurrentUser(user)),
+//       err => dispatch(receiveErrors(err.responseJSON)))
+// );
+//
+
+
+export const signOut = () => dispatch => {
+  ApiUtil.signOut()
+    .then(() => {
+      dispatch(receiveCurrentUser(null);
+    }), errors => {
+      console.log(errors);
+    })
+}
+// export const logout = () => dispatch => (
+//   APIUtil.logout().then(user => dispatch(receiveCurrentUser(null)))
+// );
+//
+
+export const receiveCurrentUser = currentUser => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser: currentUser
+  }
+}
+// export const receiveCurrentUser = currentUser => ({
+//   type: RECEIVE_CURRENT_USER,
+//   currentUser
+// });
+//
+
+export const receiveErrors = errors => {
+  return {
+    type: RECEIVE_ERRORS,
+    errors: errors
+  }
+}
+// export const receiveErrors = errors => ({
+//   type: RECEIVE_ERRORS,
+//   errors
+// });
