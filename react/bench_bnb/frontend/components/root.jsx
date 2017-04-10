0f5cad44243d7ad7c584b665f1ce9dc54e7b7db8
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-09 21:29:42 -0700

    break everything

--- react/bench_bnb/frontend/components/root.jsx
+++ react/bench_bnb/frontend/components/root.jsx
@@ -1,12 +1,12 @@
-
 import React from 'react';
 import { Provider } from 'react-redux';
 import App from './app';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
-    <App />
+    <Router history={ hashHistory }>
+      // Routes go here...
+    </Router>
   </Provider>
 );
-
-export default Root;
