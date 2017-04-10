9de19806cd8b9f689ff3d04e833ebc8efe841852
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-09 20:07:32 -0700

    mid phase 2

--- react/bench_bnb/frontend/components/root.jsx
+++ react/bench_bnb/frontend/components/root.jsx
@@ -1,13 +1,12 @@
+
 import React from 'react';
+import { Provider } from 'react-redux';
+import App from './app';
 
-const Root = ({ store }) => {
-  return (
-    <div>
-      <p>root component, current store:</p>
-      <p>{console.log(store.getState())}</p>
-    </div>
-
-  );
-};
+const Root = ({ store }) => (
+  <Provider store={ store }>
+    <App />
+  </Provider>
+);
 
 export default Root;
