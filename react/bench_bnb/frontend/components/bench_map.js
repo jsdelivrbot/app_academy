3cab63e4975aee69e89c9bb87c11105028021810
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-11 16:40:53 -0700

    write BenchMap

---
+++ react/bench_bnb/frontend/components/bench_map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BemchMap extends React.Component {
+
+  render() {
+    return (
+      <div id='map-container' ref='map'>Map</div>
+    );
+  }
+
+}
+
+export default BemchMap;
