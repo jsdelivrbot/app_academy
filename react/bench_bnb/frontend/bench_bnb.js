356a47cff70a004d6de7ba3376ae296dec8ee901
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-11 10:17:05 -0700

    make everyting work

--- react/bench_bnb/frontend/bench_bnb.js
+++ react/bench_bnb/frontend/bench_bnb.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 import { signIn, signOut, signUp } from './actions/session_actions';
+import { fetchBenches } from './util/bench_api_util';
 
 window.store = configureStore();
 window.signOut = signOut;
 window.signIn = signIn;
 window.signUp = signUp;
+window.fetchBenches = fetchBenches;
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -18,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
 });
