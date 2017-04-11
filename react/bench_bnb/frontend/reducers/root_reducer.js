5efe02b8d03c476a5322040e07d459270d7f1a60
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-11 14:10:37 -0700

    write BenchesReducer

--- react/bench_bnb/frontend/reducers/root_reducer.js
+++ react/bench_bnb/frontend/reducers/root_reducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+
+import benchesReducer from './benches_reducer';
 import SessionReducer from './session_reducer';
 
-const rootReducer = combineReducers({
+const RootReducer = combineReducers({
+  benches: benchesReducer,
   session: SessionReducer
 });
 
-export default rootReducer;
+export default RootReducer;
