71fc6a851f02ec2334d5345f7a47ea55a0b0ac03
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-10 19:06:08 -0700

    make everything good enough

--- react/bench_bnb/frontend/components/app.js
+++ react/bench_bnb/frontend/components/app.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Greeting from './greeting';
 
 const App = (props) => {
-  console.log("props:");
-  console.log(props);
   return (
     <div>
       <h1>Bench BnB</h1>
