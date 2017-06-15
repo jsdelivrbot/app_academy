e13be7d0a3ed6be4f50a9651542214e7ab23ca03
Nicole Hemenway <nkhemenway@gmail.com>
2017-06-15 10:09:30 -0700

    fix bug in initial range calculation

--- rb/hemenway_perfect_bits2.rb
+++ rb/hemenway_perfect_bits2.rb
@@ -4,8 +4,6 @@ def binarify(base_ten_num)
   base_ten_num.to_s(2)
 end
 
-# p binarify(3)
-
 def base_tenify(base_two_num)
   base_two_num.to_i(2)
 end
@@ -212,24 +210,26 @@ end
 # p prev_permutations_count(120)# == 19
 # p prev_permutations_count(267)# == 1
 
+# num1 = 9223372036854770000
+# num2 = 9223372036854775808
+
 def count_perms_in_initial_range_noninclusive(num1, num2)
   #skip if there is no initial range
   return 0 if is_binary_base?(num1)
   count = 0
-
+# debugger
   #narrow down range to actually be the initial range, cutting off the mid and/or final range
 
   # num1 is in the middle of a permutation set
   num1 = is_perfect_bit?(num1) ? num1 : next_perfect_bit(num1)
+  return 0 if num2 <= num1
 
   # num2 is the end, which should be non_inclusive
   num2 = next_binary_base(num1)
 
   prev_num1_base = prev_binary_base(num1)
 
-  total_perm_count = count_perms_in_mid_range_noninclusive(prev_num1_base, num2)
-  perm_count_minus_current_missing_set = total_perm_count - prev_permutations_count(num1)
-  count = perm_count_minus_current_missing_set
+  count = count_perms_in_mid_range_noninclusive(prev_num1_base, num2) - prev_permutations_count(num1)
 
 
   current_sq = prev_perfect_square(binary_ones_count(num1))
@@ -243,8 +243,7 @@ def count_perms_in_initial_range_noninclusive(num1, num2)
   count
 end
 
-# p count_perms_in_initial_range_noninclusive(23,36) #==
-# p count_perms_in_initial_range_noninclusive(23,32) #==4
+# p count_perms_in_initial_range_noninclusive(30000, 32768) #== 450
 
 def count_perms_in_final_range_inclusive(num1, num2)
   initial_num_in_final_range = final_base_ten_binary_base_in_range(num1, num2)
@@ -254,12 +253,14 @@ def count_perms_in_final_range_inclusive(num1, num2)
   next_binary_base_beyond_range = next_binary_base(num2)
 
   count = 0
-
+# debugger
   #count number of perms that would exist if the range end was actually the next binary base
   count += count_perms_in_mid_range_noninclusive(initial_num_in_final_range, next_binary_base_beyond_range)
 
   #subtract perms in initial range of that
   count -= count_perms_in_initial_range_noninclusive(final_num_in_final_range, next_binary_base_beyond_range)
+
+  count += 1 if final_num_in_final_range == num2
   count
 end
 
@@ -275,7 +276,7 @@ end
 
 def perfect_bits(num1, num2)
   count = 0
-# debugger
+
   if within_same_base_range?(num1, num2)
     prev_binary_base_before_range = prev_binary_base(num1)
     next_binary_base_beyond_range = next_binary_base(num2)
@@ -297,39 +298,47 @@ def perfect_bits(num1, num2)
   count
 end
 
-p '---initial range---'
-p perfect_bits(10, 16)# == 2
-p perfect_bits(100, 128)# == 10
-p perfect_bits(30000, 32768)# == 651
-
-p '---mid range---'
-p perfect_bits(2, 4)# == 2
-p perfect_bits(16, 32)# == 6
-p perfect_bits(128, 256)# == 37
-p perfect_bits(16384, 32768)# == 3369
-
-p '---final range, inclusive---'
-p perfect_bits(4, 11)# == 1
-p perfect_bits(16, 28)# == 2
-p perfect_bits(32, 46)# == 5
-p perfect_bits(16384, 30000)# == 2719
+# p '---initial range---'
+# p perfect_bits(10, 16)# == 2
+# p perfect_bits(100, 128)# == 10
+# p perfect_bits(1000, 1024)# == 6
+# p perfect_bits(30000, 32768)# == 651 => 650
+# p perfect_bits(9223372036854770000, 9223372036854775808)#==1
+#
+# p '---mid range---'
+# p perfect_bits(2, 4)# == 2
+# p perfect_bits(16, 32)# == 6
+# p perfect_bits(128, 256)# == 37
+# p perfect_bits(16384, 32768)# == 3369
+# p perfect_bits(2, 9223372036854775808)#==734248687150654971
+# p perfect_bits(128, 1099511627776)#==103351068365
+#
+# p '---final range, inclusive---'
+# p perfect_bits(4, 11)# == 2
+# p perfect_bits(16, 28)# == 2
+# p perfect_bits(32, 46)# == 5
+# p perfect_bits(16384, 30000)# == 2719
+p perfect_bits(9223372036854775807, 9223372036854775815)#==2 => 786208248064212964
+num1 = 9223372036854775807
+num2 = 9223372036854775815
+p count_perms_in_initial_range_noninclusive(num1, num2)
+p count_perms_in_mid_range_noninclusive(num1, num2)
+p count_perms_in_final_range_inclusive(num1, num2)
 
 p '---initial+mid range, non-inclusive---'
 p perfect_bits(10, 32)# == 7
 p perfect_bits(30, 64)# == 13
-p perfect_bits(100, 256)# == 45
+p perfect_bits(100, 256)# == 45 => 46
 
 p '---mid+final range, inclusive---'
 p perfect_bits(16, 39)#==7
-p perfect_bits(16, 40)#==7
+p perfect_bits(16, 40)#==7 => 6
 p perfect_bits(16, 43)#==8
-p perfect_bits(16, 44)#==8
+p perfect_bits(16, 44)#==8 => 7
 p perfect_bits(16, 45)#==9
 p perfect_bits(16, 46)#==10
-p perfect_bits(16, 47)#==10
-p perfect_bits(16, 140)#==40
+p perfect_bits(16, 47)#==10 =>9
+p perfect_bits(16, 140)#==40 =>39
 
-p perfect_bits(17,32)#==7
-p perfect_bits(10, 33)#==7
-p perfect_bits(200, 300)#==29
-p perfect_bits(200, 30000)#==5669
+
+# p next_perfect_bit(9223372036854775808)
