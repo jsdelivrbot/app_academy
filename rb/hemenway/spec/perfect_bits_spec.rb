397ca959cebf713428307a46b54ce56781ebe36d
Nicole Hemenway <nkhemenway@gmail.com>
2017-06-15 12:57:06 -0700

    add helper method specs

--- rb/hemenway/spec/perfect_bits_spec.rb
+++ rb/hemenway/spec/perfect_bits_spec.rb
@@ -1,7 +1,8 @@
 require 'rspec'
 require 'perfect_bits'
+require 'perfect_bits'
 
-describe "basic helper methods" do
+describe "base two - base ten conversion methods" do
   describe "#binarify" do
     it "returns a string" do
       expect(binarify(1234)).to be_kind_of(String)
@@ -19,8 +20,77 @@ describe "basic helper methods" do
       expect(base_tenify('10011010010')).to be(1234)
     end
   end
+end
 
-  describe "#factorial" do
+describe "perfect square helper methods" do
+  describe "#is_perfect_square?" do
+    it "returns correct value" do
+      expect(is_perfect_square?(2*2)).to be(true)
+      expect(is_perfect_square?(1234*1234)).to be(true)
+    end
+  end
+
+  describe "#next_perfect_square" do
+    it "returns correct value" do
+      expect(next_perfect_square(2*2)).to be(3*3)
+      expect(next_perfect_square(1234*1234)).to be(1235*1235)
+    end
+  end
+
+  describe "#prev_perfect_square" do
+    it "returns correct value" do
+      expect(prev_perfect_square(3*3)).to be(2*2)
+      expect(prev_perfect_square(1235*1235)).to be(1234*1234)
+    end
+  end
+end
+
+describe "perfect bit helper methods" do
+  describe "#is_perfect_bit?" do
+    it "returns correct value" do
+      expect(is_perfect_bit?(4)).to be(true)
+      expect(is_perfect_bit?(523)).to be(true)
+      expect(is_perfect_bit?(1099511627776)).to be(true)
+    end
+  end
+
+  describe "#next_perfect_bit" do
+    it "returns correct value" do
+      expect(is_perfect_bit?(4)).to be(true)
+      expect(is_perfect_bit?(523)).to be(true)
+      expect(is_perfect_bit?(1099511627776)).to be(true)
+    end
+  end
+
+  describe "#prev_perfect_bit" do
+    it "returns correct value" do
+      expect(is_perfect_bit?(4)).to be(true)
+      expect(is_perfect_bit?(523)).to be(true)
+      expect(is_perfect_bit?(1099511627776)).to be(true)
+    end
+  end
+end
+
+describe "binary base helper methods" do
+  describe "#is_binary_base?" do
+    it "returns correct value" do
+      expect(is_binary_base?(2**0)).to be(true)
+      expect(is_binary_base?(2**2345)).to be(true)
+      expect(is_binary_base?(2**2345 + 1)).to be(false)
+    end
+  end
+  describe "#next_binary_base" do
+    it "returns correct value" do
+    end
+  end
+  describe "#prev_binary_base" do
+    it "returns correct value" do
+    end
+  end
+end
+
+describe "combinatoric helper methods" do
+    describe "#factorial" do
     it "handles base case properly" do
       expect(factorial(0)).to be(1)
       expect(factorial(1)).to be(1)
@@ -30,49 +100,12 @@ describe "basic helper methods" do
       expect(factorial(20)).to be(2432902008176640000)
     end
   end
+end
 
+describe "other helper methods" do
   describe "#binary_order_of_magnitude" do
     it "returns correct value" do
-      # expect(factorial(4)).to be(4*3*2*1)
-      # expect(factorial(20)).to be(2432902008176640000)
+      expect(binary_order_of_magnitude(1234)).to be(10)
     end
-  end
-end
-
-describe "perfect square helper methods" do
-  it "#is_perfect_square?" do
-    expect(is_perfect_square?(2*2)).to be(true)
-    expect(is_perfect_square?(1234*1234)).to be(true)
-  end
-  it "#next_perfect_square" do
-    expect(next_perfect_square(2*2)).to be(3*3)
-    expect(next_perfect_square(1234*1234)).to be(1235*1235)
-  end
-  it "#prev_perfect_square" do
-    expect(prev_perfect_square(3*3)).to be(2*2)
-    expect(prev_perfect_square(1235*1235)).to be(1234*1234)
-  end
-end
-
-describe "perfect bit helper methods" do
-  it "#is_perfect_bit?" do
-    expect(is_perfect_bit?(4)).to be(true)
-    expect(is_perfect_bit?(523)).to be(true)
-    expect(is_perfect_bit?(1099511627776)).to be(true)
-  end
-  it "#next_perfect_bit" do
-  end
-  it "#prev_perfect_bit" do
-  end
-end
-
-describe "binary base helper methods" do
-  it "#is_binary_base?" do
-    # expect(is_perfect_square?(4)).to be(true)
-    # expect(is_perfect_square?(2500)).to be(true)
-  end
-  it "#next_binary_base" do
-  end
-  it "#prev_binary_base" do
   end
 end
