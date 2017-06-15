f662359d17076ce6f540354e01d8c1d2e344c809
Nicole Hemenway <nkhemenway@gmail.com>
2017-06-15 14:50:57 -0700

    break out into separate helper and main method files

---
+++ rb/hemenway/lib/helpers.rb
@@ -0,0 +1,190 @@
+
+def binarify(base_ten_num)
+  base_ten_num.to_s(2)
+end
+
+def base_tenify(base_two_num)
+  base_two_num.to_i(2)
+end
+
+def binary_ones_count(base_ten_num)
+  binarify(base_ten_num).count('1')
+end
+
+def factorial(n)
+  (1..n).inject(:*) || 1
+end
+
+def binary_order_of_magnitude(base_ten_num)
+  binarify(base_ten_num).chars.count - 1
+end
+
+def is_perfect_square?(num)
+  return false if num < 1
+  Math.sqrt(num) % 1 === 0
+end
+
+def is_perfect_bit?(base_ten_num)
+  is_perfect_square?(binary_ones_count(base_ten_num))
+end
+
+def next_perfect_bit(base_ten_num, is_initial_recursion = true)
+  return 1 if base_ten_num < 1
+
+  if is_initial_recursion || !is_perfect_bit?(base_ten_num)
+    next_num = base_ten_num + 1
+    return next_perfect_bit(next_num, false)
+
+  # cut out when hits a perfect bit that isn't the initial num
+  elsif is_perfect_bit?(base_ten_num)
+    return base_ten_num
+  end
+end
+
+# p next_perfect_bit(33)#==39
+# p next_perfect_bit(16)#==23
+
+def prev_perfect_bit(base_ten_num, is_initial_recursion = true)
+  return nil if base_ten_num < 1
+
+  if is_initial_recursion || !is_perfect_bit?(base_ten_num)
+    prev_num = base_ten_num - 1
+    return prev_perfect_bit(prev_num, false)
+
+  # cut out when hits a perfect bit that isn't the initial num
+  elsif is_perfect_bit?(base_ten_num)
+    return base_ten_num
+  end
+end
+
+# p is_perfect_bit?(30000)
+# p is_perfect_bit?(29999)
+# p is_perfect_bit?(29998)
+# p prev_perfect_bit(30000)#==29998
+# p prev_perfect_bit(16)#==15
+
+def next_perfect_square(num)
+  return 1 if num < 1
+  is_perfect_square?(num) ? next_perfect_square(num + 1) : Math.sqrt(num).ceil**2
+end
+
+def prev_perfect_square(num)
+  return nil if num <= 1
+  is_perfect_square?(num) ? prev_perfect_square(num - 1) : Math.sqrt(num).floor**2
+end
+
+def is_binary_base?(num)
+  binarify(num).count('1') == 1
+end
+
+def next_binary_base(base_ten_num)
+  return 0 if base_ten_num < 0
+  next_base = 2**(binary_order_of_magnitude(base_ten_num) + 1)
+end
+
+def prev_binary_base(base_ten_num)
+  return nil if base_ten_num < 1
+  return 0 if base_ten_num === 1
+  return base_ten_num - 1 if base_ten_num < 3
+
+  prev_base = 2**(binary_order_of_magnitude(base_ten_num))
+  prev_base === base_ten_num ? prev_binary_base(prev_base - 1) : prev_base
+end
+
+def initial_base_ten_binary_base_in_range(num1, num2)
+  return num1 if is_binary_base?(num1)
+  next_base = next_binary_base(num1)
+  return next_base if next_base <= num2
+  nil
+end
+
+def final_base_ten_binary_base_in_range(num1, num2)
+  return num2 if is_binary_base?(num2)
+  prev_base = prev_binary_base(num2)
+  return prev_base if prev_base >= num1
+  nil
+end
+
+#how many 3-digit unique permutation sets exist with n possible nums
+def uniq_permutations_count(slots_count, nums_count)
+  uniq_perms_count = factorial(nums_count)/factorial(nums_count - slots_count)
+
+  #uniq perm sets
+  uniq_perms_count/factorial(slots_count)
+end
+
+def uniq_permutations_count_w_set_ones_and_zeroes(ones_count, zeroes_count)
+  factorial(ones_count + zeroes_count)/(factorial(ones_count) * factorial(zeroes_count))
+end
+
+def count_zeroes_before_first_hanging_one(base_two_num)
+  return 0 unless base_two_num.index('0')
+  num_with_initial_ones_removed = base_two_num.slice((base_two_num.index('0'))..base_two_num.length)
+  first_one_idx = num_with_initial_ones_removed.index('1')
+  first_one_idx ? first_one_idx + 1 : 1
+end
+
+def dynamic_chunk(base_two_num)
+  return '' unless base_two_num.index('0')
+
+  #remove initial ones
+  num = base_two_num.slice((base_two_num.index('0'))..base_two_num.length)
+
+  first_one_idx = num.index('1')
+
+  if first_one_idx
+
+    # change first 1 to 0
+    num[first_one_idx] = '0'
+
+    #slice off first 0, because 1 will have shifted over there
+    return num.slice(1..num.length)
+  else
+    return num
+  end
+
+end
+
+def remove_index(arr, idx)
+  arr.slice(0...idx) + arr.slice((idx + 1)..arr.length)
+end
+
+# given '110101' in a sorted list of permutations, how many perms precede it?
+def prev_permutations_count(base_ten_num)
+  base_two_num = binarify(base_ten_num)
+  bits_arr = base_two_num.chars.slice(1..base_two_num.length)
+  count = 0
+
+  idx = 0
+
+  until bits_arr.length <= 1
+    if idx.to_s === bits_arr.first
+      bits_arr.shift
+      idx = 0
+    else
+      dynamic_chunk = bits_arr.slice((idx + 1)..bits_arr.length)
+      ones_count = dynamic_chunk.count('1')
+      zeroes_count = dynamic_chunk.count('0')
+
+      # add to ones count if idx == 0, otherwise subtract
+      # add to zeroes count if idx = 1, otherwise subtract
+      if idx === 0
+        ones_count += 1
+        zeroes_count -= 1
+      else
+        ones_count -= 1
+        zeroes_count += 1
+      end
+
+      count += uniq_permutations_count_w_set_ones_and_zeroes(ones_count, zeroes_count)
+      idx += 1
+    end
+  end
+
+  count
+end
+
+
+def within_same_base_range?(num1, num2)
+  (next_binary_base(num1) > num2) && (prev_binary_base(num2) < num1)
+end
--- rb/hemenway/spec/perfect_bits_spec.rb
+++ rb/hemenway/spec/perfect_bits_spec.rb
@@ -1,227 +1,17 @@
 require 'rspec'
+require 'helpers'
 require 'perfect_bits'
-
-describe "base two <=> base ten conversion methods" do
-  describe "#binarify" do
-    it "returns a string" do
-      expect(binarify(1234)).to be_kind_of(String)
-    end
-    it "returns correct value" do
-      expect(binarify(1234)).to eq('10011010010')
-    end
-  end
-
-  describe "#base_tenify" do
-    it "returns an integer" do
-      expect(base_tenify('10011010010')).to be_kind_of(Integer)
-    end
-    it "returns correct value" do
-      expect(base_tenify('10011010010')).to be(1234)
-    end
-  end
-end
-
-describe "perfect square helper methods" do
-  describe "#prev_perfect_square" do
-    it "returns correct value" do
-      expect(prev_perfect_square(3*3)).to be(2*2)
-      expect(prev_perfect_square(1235*1235)).to be(1234*1234)
-    end
-  end
-
-  describe "#is_perfect_square?" do
-    it "returns correct value" do
-      expect(is_perfect_square?(2*2)).to be(true)
-      expect(is_perfect_square?(1234*1234)).to be(true)
-    end
-  end
-
-  describe "#next_perfect_square" do
-    it "returns correct value" do
-      expect(next_perfect_square(2*2)).to be(3*3)
-      expect(next_perfect_square(1234*1234)).to be(1235*1235)
-    end
-  end
-end
-
-describe "perfect bit helper methods" do
-  describe "#prev_perfect_bit" do
-    it "returns correct value" do
-      expect(prev_perfect_bit(4)).to be(2)
-      expect(prev_perfect_bit(523)).to be(519)
-      expect(prev_perfect_bit(1099511627776)).to be(1099511627760)
-    end
-  end
-
-  describe "#is_perfect_bit?" do
-    it "returns correct value" do
-      expect(is_perfect_bit?(4)).to be(true)
-      expect(is_perfect_bit?(523)).to be(true)
-      expect(is_perfect_bit?(1099511627776)).to be(true)
-
-      expect(is_perfect_bit?(3)).to be(false)
-      expect(is_perfect_bit?(520)).to be(false)
-      expect(is_perfect_bit?(1099511627775)).to be(false)
-    end
-  end
-
-  describe "#next_perfect_bit" do
-    it "returns correct value" do
-      expect(next_perfect_bit(4)).to be(8)
-      expect(next_perfect_bit(512)).to be(519)
-      expect(next_perfect_bit(1099511627775)).to be(1099511627776)
-    end
-  end
-end
-
-describe "binary base helper methods" do
-  describe "#prev_binary_base" do
-    it "handles base cases" do
-      expect(prev_binary_base(0)).to be(nil)
-      expect(prev_binary_base(1)).to be(0)
-      expect(prev_binary_base(2)).to be(1)
-    end
-    it "returns correct value with small input value" do
-      expect(prev_binary_base(3)).to be(2)
-      expect(prev_binary_base(4)).to be(2)
-      expect(prev_binary_base(5)).to be(4)
-      expect(prev_binary_base(2**10)).to eq(2**9)
-    end
-    it "returns correct value with large input value" do
-      expect(prev_binary_base(2**2345 - 1)).to eq(2**2344)
-      expect(prev_binary_base(2**2345)).to eq(2**2344)
-      expect(prev_binary_base(2**2345 + 1)).to eq(2**2345)
-    end
-  end
-
-  describe "#is_binary_base?" do
-    it "returns correct value" do
-      expect(is_binary_base?(2**0)).to be(true)
-      expect(is_binary_base?(2**2345)).to be(true)
-      expect(is_binary_base?(2**2345 + 1)).to be(false)
-    end
-  end
-
-  describe "#next_binary_base" do
-    it "handles base case properly" do
-      expect(next_binary_base(-1)).to eq(0)
-    end
-    it "returns correct value with small input value" do
-      expect(next_binary_base(2**0)).to eq(2**1)
-      expect(next_binary_base(2**9)).to eq(2**10)
-    end
-    it "returns correct value with large input value" do
-      expect(next_binary_base(2**2345 - 1)).to eq(2**2345)
-      expect(next_binary_base(2**2345)).to eq(2**2346)
-      expect(next_binary_base(2**2345 + 1)).to eq(2**2346)
-    end
-  end
-
-  describe "#initial_base_ten_binary_base_in_range" do
-    it "returns correct value with edge inputs" do
-      expect(initial_base_ten_binary_base_in_range(16, 20)).to eq(16)
-      expect(initial_base_ten_binary_base_in_range(16, 32)).to eq(16)
-      expect(initial_base_ten_binary_base_in_range(512, 9223372036854775808)).to eq(512)
-      expect(initial_base_ten_binary_base_in_range(9223372036854775808, 9223372036854776000)).to eq(9223372036854775808)
-
-      expect(initial_base_ten_binary_base_in_range(10, 16)).to eq(16)
-      expect(initial_base_ten_binary_base_in_range(500, 512)).to eq(512)
-      expect(initial_base_ten_binary_base_in_range(9223372036854775000, 9223372036854775808)).to eq(9223372036854775808)
-    end
-
-    it "returns nil when does not exist" do
-      expect(initial_base_ten_binary_base_in_range(17, 20)).to be(nil)
-      expect(initial_base_ten_binary_base_in_range(300, 511)).to be(nil)
-      expect(initial_base_ten_binary_base_in_range(9223372036854775000, 9223372036854775807)).to be(nil)
-    end
-
-    it "returns correct value with standard inputs" do
-      expect(initial_base_ten_binary_base_in_range(10, 20)).to eq(16)
-      expect(initial_base_ten_binary_base_in_range(500, 9223372036854775807)).to eq(512)
-      expect(initial_base_ten_binary_base_in_range(10, 9223372036854775807)).to eq(16)
-    end
-  end
-
-  describe "#final_binary_base_in_range" do
-    it "returns correct value with edge inputs" do
-      expect(final_base_ten_binary_base_in_range(10, 16)).to eq(16)
-      expect(final_base_ten_binary_base_in_range(300, 512)).to eq(512)
-      expect(final_base_ten_binary_base_in_range(1234, 9223372036854775808)).to eq(9223372036854775808)
-
-      expect(final_base_ten_binary_base_in_range(16, 20)).to eq(16)
-      expect(final_base_ten_binary_base_in_range(512, 600)).to eq(512)
-      expect(final_base_ten_binary_base_in_range(9223372036854775808, 9223372036854776000)).to eq(9223372036854775808)
-    end
-
-    it "returns nil when does not exist" do
-      expect(final_base_ten_binary_base_in_range(17, 20)).to be(nil)
-      expect(final_base_ten_binary_base_in_range(300, 511)).to be(nil)
-      expect(final_base_ten_binary_base_in_range(9223372036854775000, 9223372036854775807)).to be(nil)
-    end
-
-    it "returns correct value with standard inputs" do
-      expect(final_base_ten_binary_base_in_range(10, 20)).to eq(16)
-      expect(final_base_ten_binary_base_in_range(500, 9223372036854776000)).to eq(9223372036854775808)
-    end
-  end
-end
-
-describe "combinatoric helper methods" do
-  describe "#factorial" do
-    it "handles base case properly" do
-      expect(factorial(0)).to be(1)
-      expect(factorial(1)).to be(1)
-    end
-    it "returns correct value" do
-      expect(factorial(4)).to be(24)
-      expect(factorial(20)).to be(2432902008176640000)
-    end
-  end
-
-  describe "#uniq_permutations_count" do
-    # it "handles base case properly" do
-    #   # expect(factorial(0)).to be(1)
-    #   # expect(factorial(1)).to be(1)
-    # end
-    # it "returns correct value" do
-    #   # expect(factorial(4)).to be(24)
-    #   # expect(factorial(20)).to be(2432902008176640000)
-    # end
-  end
-
-  describe "#uniq_permutations_count_w_set_ones_and_zeroes" do
-    # it "handles base case properly" do
-    #   # expect(factorial(0)).to be(1)
-    #   # expect(factorial(1)).to be(1)
-    # end
-    # it "returns correct value" do
-    #   # expect(factorial(4)).to be(24)
-    #   # expect(factorial(20)).to be(2432902008176640000)
-    # end
-  end
-
-  describe "#prev_permutations_count" do
-    # it "handles base case properly" do
-    #   # expect(factorial(0)).to be(1)
-    #   # expect(factorial(1)).to be(1)
-    # end
-    # it "returns correct value" do
-    #   # expect(factorial(4)).to be(24)
-    #   # expect(factorial(20)).to be(2432902008176640000)
-    # end
-  end
-end
 
 describe "main methods" do
   describe "#count_perms_in_initial_range_noninclusive" do
-    # it "handles base case properly" do
-    #   # expect(factorial(0)).to be(1)
-    #   # expect(factorial(1)).to be(1)
-    # end
-    # it "returns correct value" do
-    #   # expect(factorial(4)).to be(24)
-    #   # expect(factorial(20)).to be(2432902008176640000)
-    # end
+    it "handles base case properly" do
+      expect(factorial(0)).to be(1)
+      # expect(factorial(1)).to be(1)
+    end
+    it "returns correct value" do
+      # expect(factorial(4)).to be(24)
+      # expect(factorial(20)).to be(2432902008176640000)
+    end
   end
 
   describe "#count_perms_in_mid_range_noninclusive" do
@@ -234,15 +24,26 @@ describe "main methods" do
     #   # expect(factorial(20)).to be(2432902008176640000)
     # end
   end
-end
 
-describe "#perfect_bits" do
-  # it "handles base case properly" do
-  #   # expect(factorial(0)).to be(1)
-  #   # expect(factorial(1)).to be(1)
-  # end
-  # it "returns correct value" do
-  #   # expect(factorial(4)).to be(24)
-  #   # expect(factorial(20)).to be(2432902008176640000)
-  # end
+  describe "#count_perms_in_final_range_noninclusive" do
+    # it "handles base case properly" do
+    #   # expect(factorial(0)).to be(1)
+    #   # expect(factorial(1)).to be(1)
+    # end
+    # it "returns correct value" do
+    #   # expect(factorial(4)).to be(24)
+    #   # expect(factorial(20)).to be(2432902008176640000)
+    # end
+  end
+
+  describe "#perfect_bits" do
+    # it "handles base case properly" do
+    #   # expect(factorial(0)).to be(1)
+    #   # expect(factorial(1)).to be(1)
+    # end
+    # it "returns correct value" do
+    #   # expect(factorial(4)).to be(24)
+    #   # expect(factorial(20)).to be(2432902008176640000)
+    # end
+  end
 end
